4d81bb0ea1316bcc85bc47227d0b7f7a5970b651
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2015-10-03 18:03:34 +0100

    make soul glass transparent

--- src/main/java/masterventris/soulfoundry/blocks/SoulGlass.java
+++ src/main/java/masterventris/soulfoundry/blocks/SoulGlass.java
@@ -1,5 +1,7 @@
 package masterventris.soulfoundry.blocks;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import masterventris.soulfoundry.SoulFoundry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -10,5 +12,21 @@ public class SoulGlass extends Block{
 		setBlockName(SoulFoundry.MODID + "." + "soulGlass");
 		setCreativeTab(SoulFoundry.tabSoulFoundry);
 		setBlockTextureName(SoulFoundry.MODID + ":" + "soulGlass");
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public boolean isOpaqueCube(){
+		return false;
+	}
+	
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+
+	@Override
+    public int getRenderBlockPass(){
+            return 1;
 	}
 }
